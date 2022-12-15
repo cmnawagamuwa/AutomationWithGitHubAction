0f1097b7f0630868b7e4ebb99dee961be5ec7928
cmnawagamuwa <cmnawagamuwa@gmail.com>
2022-12-15 15:06:48 +0530

    Add project files.

---
+++ Automation/UnitTest.cs
@@ -0,0 +1,29 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Automation
+{
+    public class Tests
+    {
+        public IWebDriver Driver;
+
+
+
+        [SetUp]
+        public void Setup()
+        {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("headless");
+            Driver = new ChromeDriver(chromeOptions);
+
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Driver.Navigate().GoToUrl("https://live-102914.watiapp.io/login");
+            Assert.AreEqual("WATI - WhatsApp Team Inbox1", Driver.Title);
+        }
+    }
+}
